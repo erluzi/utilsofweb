461d380c088631d3138958b53b76583cac5482e4
erluzi <747877078@qq.com>
2020-04-17 17:45:58 +0800

    feat: add utils, do test

--- packages/utils/__tests__/check.spec.ts
+++ packages/utils/__tests__/check.spec.ts
@@ -1,8 +1,15 @@
 import {isNode} from '../check'
+import {formatNumber} from '../utils'
 
 describe('utils/check', () => {
   it('should create a dom is node', () => {
     let dom = document.createElement('div')
     expect(isNode(dom)).toBe(true)
+  })
+
+  it('should right format a number to string', () => {
+    expect(formatNumber(132134.2)).toBe('132,134.20')
+    expect(formatNumber(132134)).toBe('132,134.00')
+    expect(formatNumber('132134.236')).toBe('132,134.24')
   })
 })
