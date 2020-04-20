1b3c8db28d1514f612a65a5082acb926f3c89938
erluzi <747877078@qq.com>
2020-04-20 14:35:20 +0800

    fix

---
+++ packages/utils/__tests__/utils.spec.ts
@@ -0,0 +1,9 @@
+import {formatNumber} from '../utils'
+
+describe('utils/utils', () => {
+  it('should format number works', () => {
+    expect(formatNumber(132134.2)).toBe('132,134.20')
+    expect(formatNumber(132134)).toBe('132,134.00')
+    expect(formatNumber('132134.236')).toBe('132,134.24')
+  })
+})
